79877ebb6c3a33fa30e2588ca17f8497e3943c92
jinyoung1704 <jinyoung1704@naver.com>
2021-04-07 17:59:23 +0900

    선 연결

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+//사이즈 지정
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokStyle = "##2c2c2c";
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
@@ -6,10 +14,21 @@ function stopPainting(){
     painting = false;
 }
 
+function startPainting(){
+    painting = true;
+}
+
 function onMouseMove(event){
     //console.log(event);
     const x = event.offsetX;
     const y = event.offsetY;
+    if(!painting){ 
+        ctx.beginPath();
+        ctx.moveTo(x,y); //x,y좌표로 이동
+    }else{
+        ctx.lineTo(x,y); //점과 점을 연결해줌
+        ctx.stroke();
+    }
     //console.log(x,y);
 }
 
@@ -18,6 +37,7 @@ function onMouseDown(event){
     //console.log(event);
 }
 
+/*
 function onMouseUp(event){
     stopPainting();
 }
@@ -25,10 +45,12 @@ function onMouseUp(event){
 function onmouseleave(event){
     painting = false;
 }
+*/
 
+//이벤트 등록
 if(canvas){
     canvas.addEventListener("mousemove",onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup",onMouseUp);
-    canvas.addEventListener("mouseleave",onmouseleave);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup",stopPainting);
+    canvas.addEventListener("mouseleave",stopPainting);
 }
